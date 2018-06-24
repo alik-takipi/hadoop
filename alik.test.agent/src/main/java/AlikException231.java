e6b89900dec7f330f2972877632fc6118c5b827b
Alik Elzin <2~2e%JA!=e84Mc&,H>
2018-06-24 15:42:09 +0300

    Alik test

---
+++ alik.test.agent/src/main/java/AlikException231.java
@@ -0,0 +1,6 @@
+public class AlikException231 extends RuntimeException
+{
+	public AlikException231() {
+		super("OverOps, do you see me ? " + AlikException231.class.getSimpleName());
+	}
+}
---
+++ alik.test.agent/src/main/java/AlikExceptionNew231.java
@@ -0,0 +1,6 @@
+public class AlikExceptionNew231 extends RuntimeException
+{
+	public AlikExceptionNew231() {
+		super("OverOps, do you see me ? " + AlikExceptionNew231.class.getSimpleName());
+	}
+}
